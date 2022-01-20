7d55f34136d176ea3291e046635e2f1a83b27a97
niccy266 <niccy266@gmail.com>
2022-01-21 12:09:43 +1300

    coppied basic caching code from: https://book.pythontips.com/en/latest/function_caching.html

---
+++ joinedup/joined.py
@@ -0,0 +1,21 @@
+package joinedup
+from functools import wraps
+
+def memoize(function):
+    memo = {}
+    @wraps(function)
+    def wrapper(*args):
+        try:
+            return memo[args]
+        except KeyError:
+            rv = function(*args)
+            memo[args] = rv
+            return rv
+    return wrapper
+
+@memoize
+def fibonacci(n):
+    if n < 2: return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+fibonacci(25)
